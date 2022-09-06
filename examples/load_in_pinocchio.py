5e85c3f0b0f5db01231d00d431768c0fa125d113
Stéphane Caron <stephane.caron@normalesup.org>
2022-09-06 15:51:25 +0200

    [example] Load description in Pinocchio

---
+++ examples/load_in_pinocchio.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Load a robot description, specified from the command line, in Pinocchio.
+"""
+
+import argparse
+import os
+from importlib import import_module  # type: ignore
+
+import pinocchio as pin
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("name", help="name of the robot description")
+    args = parser.parse_args()
+
+    try:
+        module = import_module(f"robot_descriptions.{args.name}")
+    except ModuleNotFoundError:
+        module = import_module(f"robot_descriptions.{args.name}_description")
+
+    robot = pin.RobotWrapper.BuildFromURDF(
+        filename=module.URDF_PATH,
+        package_dirs=[os.path.dirname(module.PATH)],
+        root_joint=pin.JointModelFreeFlyer(),
+    )
+
+    print(f"Robot successfully loaded as {robot}")
