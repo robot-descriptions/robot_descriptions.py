bde4346065b61c63a08819c303bc1b95f6ac8c31
Stéphane Caron <stephane.caron@normalesup.org>
2022-09-12 15:43:08 +0200

    Add Poppy Ergo Jr description

--- robot_descriptions/_descriptions.py
+++ robot_descriptions/_descriptions.py
@@ -105,6 +105,7 @@ DESCRIPTIONS: Dict[str, Description] = {
     "panda_description": Description(Format.URDF),
     "panda_mj_description": Description(Format.MJCF),
     "pepper_description": Description(Format.URDF),
+    "poppy_ergo_jr_description": Description(Format.URDF),
     "poppy_torso_description": Description(Format.URDF),
     "pr2_description": Description(Format.URDF),
     "reachy_description": Description(Format.URDF),
