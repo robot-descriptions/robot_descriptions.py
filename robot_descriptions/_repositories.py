d5af9cd34f63c4218ee4372e34c00a5c47c594ea
Stéphane Caron <stephane.caron@normalesup.org>
2022-09-12 15:14:52 +0200

    Match cache paths with package name when it helps PyBullet

--- robot_descriptions/_repositories.py
+++ robot_descriptions/_repositories.py
@@ -44,136 +44,136 @@ REPOSITORIES: Dict[str, Repository] = {
     "anymal_b_simple_description": Repository(
         url="https://github.com/ANYbotics/anymal_b_simple_description.git",
         commit="988b5df22b84761bdf08111b1c2ccc883793f456",
-        cache_path="ANYbotics/anymal_b_simple_description",
+        cache_path="anymal_b_simple_description",
     ),
     "anymal_c_simple_description": Repository(
         url="https://github.com/ANYbotics/anymal_c_simple_description.git",
         commit="f160b8f7fed840c47a6febe8e2bc78b32bf43a68",
-        cache_path="ANYbotics/anymal_c_simple_description",
+        cache_path="anymal_c_simple_description",
     ),
     "baxter_common": Repository(
         url="https://github.com/RethinkRobotics/baxter_common.git",
         commit="v1.2.0",
-        cache_path="RethinkRobotics/baxter_common",
+        cache_path="baxter_common",
     ),
     "bullet3": Repository(
         url="https://github.com/bulletphysics/bullet3.git",
         commit="3.24",
-        cache_path="bulletphysics/bullet3",
+        cache_path="bullet3",
     ),
     "cassie_description": Repository(
         url="https://github.com/UMich-BipedLab/cassie_description.git",
         commit="96323f3d0cc2cb7101cb92d7fcf4650abdcb2e81",
-        cache_path="UMich-BipedLab/cassie_description",
+        cache_path="BipedLab/cassie_description",
     ),
     "cassie_mj_description": Repository(
         url="https://github.com/rohanpsingh/cassie_mj_description.git",
         commit="fcc3775453e4da6797cd4eacd3d8321d9906755a",
-        cache_path="rohanpsingh/cassie_mj_description",
+        cache_path="cassie_mj_description",
     ),
     "drake": Repository(
         url="https://github.com/RobotLocomotion/drake.git",
         commit="v1.7.0",
-        cache_path="RobotLocomotion/drake",
+        cache_path="drake",
     ),
     "eDO_description": Repository(
         url="https://github.com/Comau/eDO_description.git",
         commit="17b3f92f834746106d6a4befaab8eeab3ac248e6",
-        cache_path="Comau/edo_sim",
+        cache_path="edo_sim",  # match package name
     ),
     "example-robot-data": Repository(
         url="https://github.com/Gepetto/example-robot-data.git",
         commit="v4.0.1",
-        cache_path="Gepetto/example-robot-data",
+        cache_path="example-robot-data",
     ),
     "GingerURDF": Repository(
         url="https://github.com/Rayckey/GingerURDF.git",
         commit="6a1307cd0ee2b77c82f8839cdce3a2e2eed2bd8f",
-        cache_path="Rayckey/gingerurdf",
+        cache_path="gingerurdf",  # match package name
     ),
     "gym-pybullet-drones": Repository(
         url="https://github.com/utiasDSL/gym-pybullet-drones.git",
         commit="v1.0.0",
-        cache_path="utiasDSL/gym-pybullet-drones",
+        cache_path="gym-pybullet-drones",
     ),
     "icub-models": Repository(
         url="https://github.com/robotology/icub-models.git",
         commit="v1.25.0",
-        cache_path="robotology/icub-models",
+        cache_path="icub-models",
     ),
     "jvrc_description": Repository(
         url="https://github.com/stephane-caron/jvrc_description.git",
         commit="v1.1.0",
-        cache_path="stephane-caron/jvrc_description",
+        cache_path="jvrc_description",
     ),
     "jvrc_mj_description": Repository(
         url="https://github.com/isri-aist/jvrc_mj_description.git",
         commit="0f0ce7daefdd66c54e0909a6bf2c22154844f5f3",
-        cache_path="isri-aist/jvrc_mj_description",
+        cache_path="jvrc_mj_description",
     ),
     "mini_cheetah_urdf": Repository(
         url="https://github.com/Derek-TH-Wang/mini_cheetah_urdf.git",
         commit="1988bceb26e81f28594a16e7d5e6abe5cbb27ace",
-        cache_path="Derek-TH-Wang/mini_cheetah_urdf",
+        cache_path="mini_cheetah_urdf",
     ),
     "mujoco_menagerie": Repository(
         url="https://github.com/deepmind/mujoco_menagerie.git",
         commit="2665e9bc0f476e5f505ff929d758ed36f8618fa8",
-        cache_path="deepmind/mujoco_menagerie",
+        cache_path="mujoco_menagerie",
     ),
     "pepper_description": Repository(
         url="https://github.com/jrl-umi3218/pepper_description.git",
         commit="cd9715bb5df7ad57445d953db7b1924255305944",
-        cache_path="jrl-umi3218/pepper_description",
+        cache_path="pepper_description",
     ),
     "reachy_description": Repository(
         url="https://github.com/aubrune/reachy_description.git",
         commit="release-1.0.0",
-        cache_path="aubrune/reachy_description",
+        cache_path="reachy_description",
     ),
     "robot-assets": Repository(
         url="https://github.com/ankurhanda/robot-assets.git",
         commit="12f1a3c89c9975194551afaed0dfae1e09fdb27c",
-        cache_path="ankurhanda/robot-assets",
+        cache_path="robot-assets",
     ),
     "roboschool": Repository(
         url="https://github.com/openai/roboschool.git",
         commit="1.0.49",
-        cache_path="openai/roboschool",
+        cache_path="roboschool",
     ),
     "robotiq_arg85_description": Repository(
         url="https://github.com/a-price/robotiq_arg85_description.git",
         commit="a65190bdbb0666609fe7e8c3bb17341e09e81625",
-        cache_path="a-price/robotiq_arg85_description",
+        cache_path="robotiq_arg85_description",
     ),
     "romeo_robot": Repository(
         url="https://github.com/ros-aldebaran/romeo_robot.git",
         commit="0.1.5",
-        cache_path="ros-aldebaran/romeo_robot",
+        cache_path="romeo_robot",
     ),
     "simple_humanoid_description": Repository(
         url="https://github.com/laas/simple_humanoid_description.git",
         commit="v1.1.0",
-        cache_path="laas/simple_humanoid_description",
+        cache_path="simple_humanoid_description",
     ),
     "talos-data": Repository(
         url="https://github.com/stack-of-tasks/talos-data.git",
         commit="v2.0.0",
-        cache_path="stack-of-tasks/talos-data",
+        cache_path="talos_data",  # match package name
     ),
     "unitree_mujoco": Repository(
         url="https://github.com/unitreerobotics/unitree_mujoco.git",
         commit="f3300ff1bf0ab9efbea0162717353480d9b05d73",
-        cache_path="unitreerobotics/unitree_mujoco",
+        cache_path="unitree_mujoco",
     ),
     "unitree_ros": Repository(
         url="https://github.com/unitreerobotics/unitree_ros.git",
         commit="92a36c7cf2fe7781adedce30cd6a6ab1456ef56c",
-        cache_path="unitreerobotics/unitree_ros",
+        cache_path="unitree_ros",
     ),
     "upkie_description": Repository(
         url="https://github.com/tasts-robots/upkie_description.git",
         commit="v1.1.0",
-        cache_path="tasts-robots/upkie_description",
+        cache_path="upkie_description",
     ),
 }
