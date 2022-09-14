4e272f6747c9625495bed480f167079c7e22e03c
Stéphane Caron <stephane.caron@normalesup.org>
2022-09-14 10:56:49 +0200

    Check that MuJoCo loads all MJCF descriptions successfully

---
+++ tests/test_mujoco.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import unittest
+
+from robot_descriptions._descriptions import DESCRIPTIONS
+from robot_descriptions.loaders.mujoco import load_robot_description
+
+
+class TestMuJoCo(unittest.TestCase):
+
+    """
+    Check that all MJCF descriptions are loaded properly in MuJoCo.
+    """
+
+    def setUp(self):
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.INFO)
+
+    @staticmethod
+    def get_test_for_description(description: str):
+        """
+        Get test function for a given description.
+
+        Args:
+            description: Name of the description.
+
+        Returns:
+            Test function for that description.
+        """
+
+        def test(self):
+            logging.info(f"Loading {description} in MuJoCo...")
+            load_robot_description(description)
+
+        return test
+
+
+for name, description in DESCRIPTIONS.items():
+    if description.has_mjcf:
+        setattr(
+            TestMuJoCo,
+            f"test_{name}",
+            TestMuJoCo.get_test_for_description(name),
+        )
