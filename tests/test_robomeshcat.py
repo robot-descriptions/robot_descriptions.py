46dae78b69d5139d3213ed70c840343908a88e09
Stéphane Caron <stephane.caron@normalesup.org>
2022-11-28 18:41:43 +0100

    WIP: Enable Upkie test again

--- tests/test_robomeshcat.py
+++ tests/test_robomeshcat.py
@@ -36,12 +36,12 @@ try:
         #     mini_cheetah = load_robot_description("mini_cheetah_description")
         #     self.assertIsNotNone(mini_cheetah)
 
-        # def test_description_with_cylinders(self):
-        #     """
-        #     Load a robot description with cylinder shapes.
-        #     """
-        #     upkie = load_robot_description("upkie_description")
-        #     self.assertIsNotNone(upkie)
+        def test_description_with_cylinders(self):
+            """
+            Load a robot description with cylinder shapes.
+            """
+            upkie = load_robot_description("upkie_description")
+            self.assertIsNotNone(upkie)
 
         # def test_collada_description(self):
         #     """
