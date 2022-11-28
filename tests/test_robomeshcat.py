4c0aca0a05a18108ebdad7992bd62dfede3eb9f1
Stéphane Caron <stephane.caron@normalesup.org>
2022-11-28 18:02:34 +0100

    Add RoboMeshCat test with Collada

--- tests/test_robomeshcat.py
+++ tests/test_robomeshcat.py
@@ -34,9 +34,21 @@ try:
             of robot descriptions.
             """
             mini_cheetah = load_robot_description("mini_cheetah_description")
-            upkie = load_robot_description("upkie_description")
             self.assertIsNotNone(mini_cheetah)
+
+        def test_description_with_cylinders(self):
+            """
+            Load a robot description with cylinder shapes.
+            """
+            upkie = load_robot_description("upkie_description")
             self.assertIsNotNone(upkie)
+
+        def test_collada_description(self):
+            """
+            Test a robot description with Collada meshes.
+            """
+            r2 = load_robot_description("r2_description")
+            self.assertIsNotNone(r2)
 
 
 except ImportError:
