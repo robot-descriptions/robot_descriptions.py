304254eb676a2e7f32dbcd3fdfce346b8f0b80dc
Stéphane Caron <stephane.caron@normalesup.org>
2022-11-29 13:43:01 +0100

    [CI] Clean up redundant RoboMeshCat tests

--- tests/loaders/test_robomeshcat.py
+++ tests/loaders/test_robomeshcat.py
@@ -28,28 +28,6 @@ class TestRoboMeshCat(unittest.TestCase):
     Check that all descriptions are loaded properly in RoboMeshCat.
     """
 
-    def test_load_robot_description(self):
-        """
-        Load a robot description with RoboMeshCat.
-
-        """
-        mini_cheetah = load_robot_description("mini_cheetah_description")
-        self.assertIsNotNone(mini_cheetah)
-
-    def test_description_with_cylinders(self):
-        """
-        Load a robot description with cylinder shapes.
-        """
-        upkie = load_robot_description("upkie_description")
-        self.assertIsNotNone(upkie)
-
-    def test_collada_description(self):
-        """
-        Test a robot description with Collada meshes.
-        """
-        r2 = load_robot_description("r2_description")
-        self.assertIsNotNone(r2)
-
     @staticmethod
     def get_test_for_description(description: str):
         """
