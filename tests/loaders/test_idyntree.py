4e9bb4ab0dd26bea37c43bdf4e54b949d07b0e10
Giulio Romualdi <giulio.romualdi@gmail.com>
2022-12-06 22:56:43 +0100

    Implement the unit test for the iDynTree loader

---
+++ tests/loaders/test_idyntree.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Giulio Romualdi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from robot_descriptions._descriptions import DESCRIPTIONS
+
+from robot_descriptions.loaders.idyntree import load_robot_description
+
+class TestiDynTree(unittest.TestCase):
+
+    """
+    Check that all descriptions are loaded properly in iDynTree.
+    """
+
+    @staticmethod
+    def get_test_for_description(description: str):
+        """
+        Get test function for a given description.
+
+        Args:
+            description: Name of the description.
+
+        Returns:
+            Test function for that description.
+        """
+
+        def test(self):
+            load_robot_description(description)
+
+        return test
+
+# Add a test function for each URDF description
+for name, description in DESCRIPTIONS.items():
+    if description.has_urdf:
+        setattr(
+            TestiDynTree,
+            f"test_{name}",
+            TestiDynTree.get_test_for_description(name),
+        )
