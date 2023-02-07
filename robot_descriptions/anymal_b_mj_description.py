758067e06f6d9f577665760154e7c93a8eecd6bb
Stéphane Caron <stephane.caron@normalesup.org>
2023-02-07 11:08:26 +0100

    WIP: Update more submodules

--- robot_descriptions/anymal_b_mj_description.py
+++ robot_descriptions/anymal_b_mj_description.py
@@ -19,11 +19,15 @@
 ANYmal B MJCF description.
 """
 
+from os import getenv as _getenv
 from os import path as _path
 
 from ._cache import clone_to_cache as _clone_to_cache
 
-REPOSITORY_PATH: str = _clone_to_cache("mujoco_menagerie")
+REPOSITORY_PATH: str = _clone_to_cache(
+    "mujoco_menagerie",
+    commit=_getenv("ROBOT_DESCRIPTION_COMMIT", None),
+)
 
 PACKAGE_PATH: str = _path.join(REPOSITORY_PATH, "anybotics_anymal_b")
 
--- robot_descriptions/barrett_hand_description.py
+++ robot_descriptions/barrett_hand_description.py
@@ -19,11 +19,15 @@
 BarrettHand description.
 """
 
+from os import getenv as _getenv
 from os import path as _path
 
 from ._cache import clone_to_cache as _clone_to_cache
 
-REPOSITORY_PATH: str = _clone_to_cache("bhand_model")
+REPOSITORY_PATH: str = _clone_to_cache(
+    "bhand_model",
+    commit=_getenv("ROBOT_DESCRIPTION_COMMIT", None),
+)
 
 PACKAGE_PATH: str = _path.join(REPOSITORY_PATH)
 
