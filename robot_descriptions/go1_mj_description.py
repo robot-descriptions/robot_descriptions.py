5424d3e394524641dfd5d15fda09c22b2421aff7
Kevin Zakka <kevinarmandzakka@gmail.com>
2024-03-26 12:35:07 -0700

    Minor unitree model fixes.

--- robot_descriptions/go1_mj_description.py
+++ robot_descriptions/go1_mj_description.py
@@ -12,12 +12,10 @@ from os import path as _path
 from ._cache import clone_to_cache as _clone_to_cache
 
 REPOSITORY_PATH: str = _clone_to_cache(
-    "unitree_mujoco",
+    "mujoco_menagerie",
     commit=_getenv("ROBOT_DESCRIPTION_COMMIT", None),
 )
 
-PACKAGE_PATH: str = _path.join(REPOSITORY_PATH, "data", "go1")
+PACKAGE_PATH: str = _path.join(REPOSITORY_PATH, "unitree_go1")
 
-MJCF_PATH: str = _path.join(PACKAGE_PATH, "xml", "go1.xml")
-
-URDF_PATH: str = _path.join(PACKAGE_PATH, "urdf", "go1.urdf")
+MJCF_PATH: str = _path.join(PACKAGE_PATH, "go1.xml")
--- robot_descriptions/go2_mj_description.py
+++ robot_descriptions/go2_mj_description.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2022 Stéphane Caron
 
-"""Go1 MJCF description."""
+"""Go2 MJCF description."""
 
 from os import getenv as _getenv
 from os import path as _path
