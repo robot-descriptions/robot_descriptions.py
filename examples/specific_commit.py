4a5c9fbb27107a0a55c2407402a94e41844342e3
Stéphane Caron <stephane.caron@inria.fr>
2024-06-04 10:11:05 +0200

    Add example to load with a specific commit

---
+++ examples/specific_commit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 Inria
+
+"""
+We can load specific commit to pin the version of any robot description using
+the ``commit`` keyword argument. In this example, we load a Universal Robots
+UR5e from a specific commit of the MuJoCo Menagerie repository that distributes
+it. This example requires MuJoCo, which is installed by ``pip install mujoco``.
+
+See also:
+    https://github.com/robot-descriptions/robot_descriptions.py/discussions/86
+"""
+
+from robot_descriptions.loaders.mujoco import load_robot_description
+
+if __name__ == "__main__":
+    model = load_robot_description(
+        "ur5e_mj_description",
+        commit="cc3868ec21bae324e6dd3b01ea185b6e6f0fa172",
+    )
+    print(f"UR5e successfully loaded in {model=}")
