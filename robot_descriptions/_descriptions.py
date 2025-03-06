5c1f96a4f76384ef885eb11821c0d5fb421c853f
Stéphane Caron <stephane.caron@inria.fr>
2025-03-06 14:30:37 +0100

    Add AgileX PiPER MJCF description

--- robot_descriptions/_descriptions.py
+++ robot_descriptions/_descriptions.py
@@ -108,6 +108,7 @@ DESCRIPTIONS: Dict[str, Description] = {
     "panda_mj_description": Description(Format.MJCF),
     "pepper_description": Description(Format.URDF),
     "piper_description": Description(Format.URDF),
+    "piper_mj_description": Description(Format.MJCF),
     "poppy_ergo_jr_description": Description(Format.URDF),
     "poppy_torso_description": Description(Format.URDF),
     "pr2_description": Description(Format.URDF),
