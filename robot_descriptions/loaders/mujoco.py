ea36f4f434e16f0d10aaa80a357356828de2669b
Justin Yu <yujustin8@gmail.com>
2025-06-02 12:26:58 -0700

    rm pdb

--- robot_descriptions/loaders/mujoco.py
+++ robot_descriptions/loaders/mujoco.py
@@ -40,8 +40,6 @@ def load_robot_description(
 
     def load_model_from_path(path):
         try:
-            import pdb; pdb.set_trace()
-
             return mujoco.MjModel.from_xml_path(path)
         except ValueError:
             print(f"{path} not found. Loading default robot model.")
