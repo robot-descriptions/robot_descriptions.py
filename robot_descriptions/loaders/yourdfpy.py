e339eded5a4be770e157fc483172b848f63889a2
Sebastian Castro <sebas.a.castro@gmail.com>
2025-06-17 22:24:08 -0400

    Add args to yourdfpy loader to optionally load visuals and collisions

--- robot_descriptions/loaders/yourdfpy.py
+++ robot_descriptions/loaders/yourdfpy.py
@@ -23,6 +23,8 @@ except ModuleNotFoundError as e:
 def load_robot_description(
     description_name: str,
     commit: Optional[str] = None,
+    load_visuals: bool = True,
+    load_collisions: bool = True,
 ) -> yourdfpy.URDF:
     """Load a robot description in yourdfpy.
 
@@ -30,6 +32,8 @@ def load_robot_description(
         description_name: Name of the robot description.
         commit: If specified, check out that commit from the cloned robot
             description repository.
+        load_visuals: If true, loads the visual meshes and scene.
+        load_collisions: If true, loads the collision meshes and scene.
 
     Returns:
         Robot model for yourdfpy.
@@ -42,4 +46,11 @@ def load_robot_description(
     if not hasattr(module, "URDF_PATH"):
         raise ValueError(f"{description_name} is not a URDF description")
 
-    return yourdfpy.URDF.load(module.URDF_PATH, mesh_dir=module.PACKAGE_PATH)
+    return yourdfpy.URDF.load(
+        module.URDF_PATH,
+        mesh_dir=module.PACKAGE_PATH,
+        build_scene_graph=load_visuals,
+        build_collision_scene_graph=load_collisions,
+        load_meshes=load_visuals,
+        load_collision_meshes=load_collisions,
+    )
