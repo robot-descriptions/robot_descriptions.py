b2adba3ba5cc50aff29920a9a15da3f49bd490c5
Stéphane Caron <stephane.caron@normalesup.org>
2026-09-12 12:41:23 +0200

    Remove utf-8 coding lines
    Python 3 uses UTF-8 as the default source-code encoding. Files are saved
    as UTF-8, thus Python does not need an explicit declaration.

--- examples/load_in_idyntree.py
+++ examples/load_in_idyntree.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 #
 # SPDX-License-Identifier: Apache-2.0
 #
--- robot_descriptions/gr1_description.py
+++ robot_descriptions/gr1_description.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 """GR-1 description."""
 
