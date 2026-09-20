a9650cdc4439a1333f7afd8225472ddc52d6b50d
gospar <antun.skuric@outlook.com>
2026-09-20 17:37:16 +0200

    a typo

--- robot_descriptions/iiwa7_epfl_description.py
+++ robot_descriptions/iiwa7_epfl_description.py
@@ -14,6 +14,6 @@ REPOSITORY_PATH: str = _clone_to_cache(
     commit=_getenv("ROBOT_DESCRIPTION_COMMIT", None),
 )
 
-PACKAGE_PATH: str = _path.join(REPOSITORY_PATH, "iiwa7_description")
+PACKAGE_PATH: str = _path.join(REPOSITORY_PATH, "iiwa_description")
 
 XACRO_PATH: str = _path.join(PACKAGE_PATH, "urdf", "iiwa7.urdf.xacro")
